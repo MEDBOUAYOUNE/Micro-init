002150878443df763711e6a74c774222730aafb2
MEDBOUAYOUNE <bouayounemoha1@gmail.com>
2025-08-07 17:08:11 +0200

    Setuping the project

---
+++ service1/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Service 1!');
+});
+
+app.listen(port, () => {
+    console.log(`Service 1 is running on http://localhost:${port}`);
+});
